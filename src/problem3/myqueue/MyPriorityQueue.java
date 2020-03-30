bae0c821b76cbca54352970528f9c2ac32f6cf45
bhaktishar26 <bhakti.sharma_cs18@gla.ac.in>
2020-03-31 01:12:38 +0530

    added methods

--- src/problem3/myqueue/MyPriorityQueue.java
+++ src/problem3/myqueue/MyPriorityQueue.java
@@ -15,5 +15,36 @@ public class MyPriorityQueue {
         head = null;
     }
 
+    public void insert(String name,int roll) {
+        Node node = new Node();
+        node.setName(name);
+        node.setRoll(roll);
+        Node temp = head;
+        if(head==null) {
+            head = node;
+        }
+        else {
+            if(head.getRoll() > roll) {
+                node.setNext(head);
+                head = temp;
+            }
+            else {
+                while(temp.getNext()!=null && temp.getNext().getRoll()<roll) {
+                    temp = temp.getNext();
+                }
+                node.setNext(temp.getNext());
+                temp.setNext(node);
+            }
+        }
+    }
+
+    public void print() {
+        Node temp = head;
+        while(temp!=null) {
+            System.out.println(temp.getName()+"\t"+temp.getRoll());
+            temp = temp.getNext();
+        }
+    }
+
 
 }
