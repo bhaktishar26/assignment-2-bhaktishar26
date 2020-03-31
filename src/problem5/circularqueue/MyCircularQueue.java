9a19a068cb0ae6c984ef76b72f547e9b45fa88ed
bhaktishar26 <bhakti.sharma_cs18@gla.ac.in>
2020-03-31 19:37:54 +0530

    added methods

--- src/problem5/circularqueue/MyCircularQueue.java
+++ src/problem5/circularqueue/MyCircularQueue.java
@@ -8,6 +8,7 @@ package problem5.circularqueue;
 
 
 import problem5.node.Node;
+import problem5.student.Student;
 
 //to implement circular queue
 public class MyCircularQueue {
@@ -21,6 +22,43 @@ public class MyCircularQueue {
         count = 0;
     }
 
+    public void enqueue(int roll,int backlog,int appearCount) {
+        Student student = new Student(roll,backlog,appearCount);
+        Node node = new Node();
+        node.setStudent(student);
 
+        if(front==null) {
+            front = node;
+        }
+        else {
+            rear.setNext(node);
+        }
+        rear = node;
+        rear.setNext(front);
+        count++;
+    }
 
+    public void removeBacklog() {
+        while(count-->0) {
+            Student temp = front.getStudent();
+            if((temp.getBacklog()-temp.getAppearCount())>0) {
+                front = front.getNext();
+                rear = rear.getNext();
+            }
+            else {
+                rear.setNext(front.getNext());
+                front = front.getNext();
+            }
+        }
+    }
+
+    public void print() {
+        Node temp = front;
+        System.out.println();
+        while(temp.getNext()!=front) {
+            System.out.println(temp.getStudent());
+            temp = temp.getNext();
+        }
+        System.out.println(temp.getStudent());
+    }
 }
